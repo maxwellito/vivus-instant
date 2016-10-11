b1d13b64948619bd05d8cd97008aacf3e88e7a61
maxwellito <duluc.maxime@gmail.com>
2016-10-12 00:36:59 +0100

    Clean up Playground controller

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,42 +1,54 @@
-// Listen for drag and drop
-document.addEventListener('drop',      dropped,        false);
-document.addEventListener('dragover',  dragOver, false);
-document.addEventListener('dragleave', dragLeave, false);
+function PlaygroundController (el) {
+  this.el = el;
+  this.svgTag = null;
+  this.svgFileName = null;
+  this.newSvgCb = null;
 
-var el = document.querySelector('.playground');
-var mySVG;
-
-function dragOver (event) {
-  event.preventDefault();
-  el.classList.add('droppin');
+  // Listen for drag and drop
+  el.addEventListener('drop',      this.dropped.bind(this),   false);
+  el.addEventListener('dragover',  this.dragOver.bind(this),  false);
+  el.addEventListener('dragleave', this.dragLeave.bind(this), false);
 }
 
-function dragLeave (event) {
-  event.preventDefault();
-  el.classList.remove('droppin');
+PlaygroundController.prototype.onNewSVG = function (callback) {
+  this.newSvgCb = callback;
 }
 
-function dropped (event) {
+PlaygroundController.prototype.dragOver = function (event) {
   event.preventDefault();
-  var data = event.dataTransfer;
+  this.el.classList.add('droppin');
+};
+
+PlaygroundController.prototype.dragLeave = function (event) {
+  event.preventDefault();
+  this.el.classList.remove('droppin');
+};
+
+PlaygroundController.prototype.dropped = function (event) {
+  event.preventDefault();
+
+  var file, data = event.dataTransfer;
   if (!data || !data.files || !data.files[0]) {
     return;
   }
 
-  console.info(data.files[0]);
-
-  var file = new FileReader();
-  file.addEventListener('load', function () {
-    console.log(file.result);
-    var encodedData = file.result;
-    encodedData = encodedData.substr(encodedData.indexOf('base64,')+7);
-    var data = atob(encodedData);
-
-    var wrapDom = document.createElement('div');
-    wrapDom.innerHTML = data;
-    mySVG = wrapDom.childNodes[0];
-    el.appendChild(mySVG)
-  }, false);
-  uploadedFileName = data.files[0].name;
+  file = new FileReader();
+  file.addEventListener('load', this.buildSVG.bind(this), false);
   file.readAsDataURL(data.files[0]);
-}
+  this.svgFileName = data.files[0].name;
+};
+
+PlaygroundController.prototype.buildSVG = function (event) {
+  var encodedData = event.currentTarget.result;
+  encodedData = encodedData.substr(encodedData.indexOf('base64,')+7);
+  var wrapDom = document.createElement('div');
+  wrapDom.innerHTML = atob(encodedData);
+
+  if (this.svg) {
+    this.el.removeChild(this.svgTag);
+    this.svgTag.remove();
+  }
+  this.svgTag = wrapDom.childNodes[0];
+  this.el.appendChild(this.svgTag);
+  this.newSvgCb && this.newSvgCb(this.svg);
+};
