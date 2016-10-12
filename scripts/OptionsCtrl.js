c25613def40a11984b57d8887fca1b5904277614
maxwellito <duluc.maxime@gmail.com>
2016-10-12 22:28:47 +0100

    Improuve form and add OptionController

---
+++ scripts/OptionsCtrl.js
@@ -0,0 +1,29 @@
+function OptionController (el) {
+  this.el = el;
+
+  el.querySelectorAll('input[x-panel]')
+    .forEach(function (input) {
+      input.addEventListener('click', function (e) {
+        var panel = document.querySelector('.' + e.currentTarget.getAttribute('x-panel'));
+        panel.style.display = e.currentTarget.checked ? 'block' : 'none';
+      });
+    })
+}
+
+OptionController.prototype.getOptions = function () {
+  var options = {};
+  this.el
+    .querySelectorAll('form *[name]')
+    .forEach((el) => {
+      if (el.type === 'radio' && el.checked) {
+        return;
+      }
+      else if (el.type === 'checkbox') {
+        options[el.name] = el.checked;
+      }
+      else if (el.value) {
+        options[el.name] = el.value;
+      }
+    });
+  return options;
+};
